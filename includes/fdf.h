32bfecdb0ecedd25506b37590d2d454ab1563591
Andreas Murtonen <amurtone@c2r6p3.hive.fi>
2019-12-18 16:10:46 +0200

    drawing 2d map

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: amurtone <amurtone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/13 11:05:38 by amurtone          #+#    #+#             */
-/*   Updated: 2019/12/17 18:25:50 by amurtone         ###   ########.fr       */
+/*   Updated: 2019/12/18 13:02:55 by amurtone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,16 +29,17 @@ typedef struct      s_fdf
     int width;
     int height;
     int **z_matrix;
+    int zoom;
+    int color;
 
-    void *mlx_ptr;
-    void *win_ptr;
+
+    void    *mlx_ptr;
+    void    *win_ptr;
 }                   t_fdf;
 
-const char    *ft_next_word(const char *s, char c, int skip);
-int     ft_count_words(const char *s, char c);
 void    read_map(char *map, t_fdf *data);
-void    fill_matrix(int *z_line, char *line);
-int     get_width(char *map);
-int     get_height(char *map);
+int		ft_count_words(const char *s, char c);
+void    brsham(float x, float y, float x1, float y1, t_fdf *data);
+void    draw_lines(t_fdf *data);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,42 +6,33 @@
 /*   By: amurtone <amurtone@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/13 13:00:24 by amurtone          #+#    #+#             */
-/*   Updated: 2019/12/17 18:27:07 by amurtone         ###   ########.fr       */
+/*   Updated: 2019/12/18 14:52:21 by amurtone         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fdf.h"
 
+int     deal_key(int key/*, t_fdf *data*/)
+{
+    ft_putnbr(key);
+    ft_putchar('\n');
+    return (0);
+}
+
 int     main(int ac, char **av)
 {
-    /*
-    void *mlx_ptr;
-    void *win_ptr;
+    t_fdf    *data;
 
-    mlx_ptr = mlx_init();   
-    win_ptr = mlx_new_window(mlx_ptr, 500, 500, "super awesome");
-    mlx_loop(mlx_ptr);
-    */
-   int i;
-   int j;
-   t_fdf *data;
- 
-   data = (t_fdf *)malloc(sizeof(t_fdf));
-   if (ac == 2)
-   {
+    if (ac == 2)
+    {
+        data = (t_fdf *)malloc(sizeof(t_fdf));
         read_map(av[1], data);
-        i = 0;
-        while (i < data->height)
-        {
-            j = 0;
-            while(j < data->width)
-            {
-                ft_putnbr(data->z_matrix[i][j]);
-                ft_putchar(' ');
-                j++;
-            }
-            i++;
-        }
+        data->mlx_ptr = mlx_init();
+        data->win_ptr = mlx_new_window(data->mlx_ptr, 1000, 1000, "fdf super awesome");
+        data->zoom = 20;
+        draw_lines(data);
+        mlx_key_hook(data->win_ptr, deal_key, data);
+        mlx_loop(data->mlx_ptr);
    }
    
 
